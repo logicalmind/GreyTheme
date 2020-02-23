da99e0b23737a1180505de3ff7b186001b0f8a8c
Logical Mind <logicalmind@gmail.com>
2020-02-23 11:46:12 -0800

    Initial checkin

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] unused)
+        {
+            var myClass = new MyClass("test", 100);
+            var tmp = myClass.MyMethod(myClass) + myClass.MyExtension();
+
+            Console.WriteLine(tmp);
+            Console.Out.WriteLine(tmp);
+        }
+    }
+
+    public class MyClass
+    {
+        private const int MyConstant = 1;
+        private string myField;
+
+        public int MyProperty { get; set; }
+
+        public MyClass(string myField, int myProperty)
+        {
+            this.myField = myField;
+            MyProperty = Math.Max(myProperty, MyConstant);
+        }
+
+        public string MyMethod(MyClass input)
+        {
+            Guid g = new Guid("test");
+            return myField + MyProperty + MyConstant + input.MyExtension() + g.ToString();
+        }
+    }
+
+    public static class MyExtentsion
+    {
+        public static string MyExtension(this MyClass str)
+        {
+            return str.ToString();
+        }
+    }
+}
